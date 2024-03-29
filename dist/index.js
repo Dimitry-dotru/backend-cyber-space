a9b0d9051423db1782560df1ebcaad644d69f3ee
Dmitry Shumskiy <ejedima260@gmail.com>
2024-03-29 19:52:45 +0200

    temporary var

--- dist/index.js
+++ dist/index.js
@@ -8,8 +8,6 @@ const passport_1 = __importDefault(require("passport"));
 const express_session_1 = __importDefault(require("express-session"));
 const passport_steam_1 = __importDefault(require("passport-steam"));
 const http_proxy_middleware_1 = require("http-proxy-middleware");
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config({ path: ".env.local" });
 const SteamStrategy = passport_steam_1.default.Strategy;
 const app = (0, express_1.default)();
 passport_1.default.serializeUser((user, done) => {
@@ -19,9 +17,9 @@ passport_1.default.deserializeUser((user, done) => {
     done(null, user);
 });
 passport_1.default.use(new SteamStrategy({
-    returnURL: process.env.backendServer + "/api/auth/steam/return",
-    realm: process.env.backendServer + "/",
-    apiKey: process.env.apiKey,
+    returnURL: "http://localhost:3000",
+    realm: "http://localhost:7069/",
+    apiKey: "BDE51B80D4D4E0257B60610C0B3FE6F6",
 }, function (identifier, profile, done) {
     process.nextTick(function () {
         profile.identifier = identifier;
@@ -29,7 +27,7 @@ passport_1.default.use(new SteamStrategy({
     });
 }));
 app.use((0, express_session_1.default)({
-    secret: 'Whatever_You_Want',
+    secret: "My_secret",
     saveUninitialized: true,
     resave: false,
     cookie: {
@@ -39,31 +37,32 @@ app.use((0, express_session_1.default)({
 app.use(passport_1.default.initialize());
 app.use(passport_1.default.session());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
-app.use('/steam', (0, http_proxy_middleware_1.createProxyMiddleware)({
+app.use("/steam", (0, http_proxy_middleware_1.createProxyMiddleware)({
     target: "https://api.steampowered.com",
     changeOrigin: true,
     pathRewrite: {
-        '^/steam': '', // Убираем '/steam' из пути
+        "^/steam": "", // Убираем "/steam" из пути
     }
 }));
-app.listen(process.env.port, () => {
-    console.log('Listening, port ' + process.env.port);
+app.listen(7069, () => {
+    console.log("Listening, port " + 7069);
 });
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.send(req.user);
     console.log(req.user);
 });
-app.get('/api/auth/steam', passport_1.default.authenticate('steam', { failureRedirect: '/' }), function (req, res) {
-    res.redirect('/');
+app.get("/api/auth/steam", passport_1.default.authenticate("steam", { failureRedirect: "/" }), function (req, res) {
+    // res.redirect("/")
+    res.redirect("http://localhost:3000");
 });
-app.get('/api/auth/steam/return', passport_1.default.authenticate('steam', { failureRedirect: '/' }), function (req, res) {
+app.get("/api/auth/steam/return", passport_1.default.authenticate("steam", { failureRedirect: "/" }), function (req, res) {
     const steamId = req.user.id;
-    const redirectUrl = `${process.env.frontendServer}/?steamId=${steamId}`;
+    const redirectUrl = `http://localhost:3000/?steamId=${steamId}`;
     res.redirect(redirectUrl);
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import passport from "passport";
 import session from "express-session";
 import passportSteam from "passport-steam";
 import { createProxyMiddleware } from "http-proxy-middleware";
-import dotenv from "dotenv";
-dotenv.config({path: ".env.local"});
 
 const SteamStrategy = passportSteam.Strategy;
 const app = express();
@@ -30,9 +28,9 @@ passport.deserializeUser((user, done) => {
 passport.use(
   new SteamStrategy(
     {
-      returnURL: process.env.backendServer + "/api/auth/steam/return",
-      realm: process.env.backendServer + "/",
-      apiKey: process.env.apiKey,
+      returnURL: "http://localhost:3000",
+      realm: "http://localhost:7069/",
+      apiKey: "BDE51B80D4D4E0257B60610C0B3FE6F6",
     },
     function (identifier, profile, done) {
       process.nextTick(function () {
@@ -44,7 +42,7 @@ passport.use(
 );
 
 app.use(session({
-	secret: 'Whatever_You_Want',
+	secret: "My_secret",
 	saveUninitialized: true,
 	resave: false,
 	cookie: {
@@ -57,36 +55,37 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
-app.use('/steam', createProxyMiddleware({
+app.use("/steam", createProxyMiddleware({
   target: "https://api.steampowered.com",
   changeOrigin: true,
   pathRewrite: {
-    '^/steam': '', // Убираем '/steam' из пути
+    "^/steam": "", // Убираем "/steam" из пути
   }
 }));
 
-app.listen(process.env.port, () => {
-	console.log('Listening, port ' + process.env.port);
+app.listen(7069, () => {
+	console.log("Listening, port " + 7069);
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
 	
 	res.send(req.user);
 	console.log(req.user)
 });
 
-app.get('/api/auth/steam', passport.authenticate('steam', {failureRedirect: '/'}), function (req, res) {
-	res.redirect('/')
+app.get("/api/auth/steam", passport.authenticate("steam", {failureRedirect: "/"}), function (req, res) {
+	// res.redirect("/")
+  res.redirect("http://localhost:3000");
 });
 
-app.get('/api/auth/steam/return', passport.authenticate('steam', {failureRedirect: '/'}), function (req, res) {
+app.get("/api/auth/steam/return", passport.authenticate("steam", {failureRedirect: "/"}), function (req, res) {
   const steamId = req.user.id;
-  const redirectUrl = `${process.env.frontendServer}/?steamId=${steamId}`;
+  const redirectUrl = `http://localhost:3000/?steamId=${steamId}`;
   res.redirect(redirectUrl);
 });
